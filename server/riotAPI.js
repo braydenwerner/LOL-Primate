8e6dbb4c9b4eedd88a8f93b8b03f2c707fe2f4da
Brayden Werner <braydenwerner0@gmail.com>
2021-04-24 10:59:15 -0700

    Added environment variables, updated API fetching

--- server/riotAPI.js
+++ server/riotAPI.js
@@ -1,36 +1,64 @@
 const axios = require('axios')
 
-const API_KEY = 'RGAPI-ba80f868-b434-41ad-b24c-7c5626fa3935'
-
 const getBySummonerName = async (summonerName) => {
+  count++
   const res = await axios.get(
-    `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${API_KEY}`
+    `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${process.env.RIOT_API_KEY}`
   )
   return res.data
 }
 
 const getLeagueEntriesForSummoner = async (summonerID) => {
   const res = await axios.get(
-    `https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/${summonerID}?api_key=${API_KEY}`
+    `https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/${summonerID}?api_key=${process.env.RIOT_API_KEY}`
   )
-
   return res.data
 }
 
+const getMatchIDs = async (puuid) => {
+  const res = await axios.get(
+    `https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=10&api_key=${process.env.RIOT_API_KEY}`
+  )
+  return res.data
+}
+
+const getMatchData = async (matchID) => {
+  const res = await axios.get(
+    `https://americas.api.riotgames.com/lol/match/v5/matches/${matchID}?api_key=${process.env.RIOT_API_KEY}`
+  )
+  return res.data
+}
+
+//   function gets data from three APIs and appends the data
+//  into one object
 const getSummonerData = async (summonerNames) => {
   const summonerData = {}
   for (summonerName of summonerNames) {
     const profileData = await getBySummonerName(summonerName)
-    let matchData = await getLeagueEntriesForSummoner(profileData.id)
 
-    matchData = matchData.filter((obj) => obj.queueType === 'RANKED_SOLO_5x5')
+    const rankedData = (
+      await getLeagueEntriesForSummoner(profileData.id)
+    ).filter((obj) => obj.queueType === 'RANKED_SOLO_5x5')
+
     summonerData[summonerName] = {
       ...profileData,
-      ...(matchData.length > 0 ? matchData[0] : {})
+      ...(rankedData.length > 0 ? rankedData[0] : {})
     }
   }
-
   return summonerData
 }
 
-module.exports = { getSummonerData }
+const getSummonerMatchData = async (summonerPUUIDs) => {
+  console.log('key: ', process.env.RIOT_API_KEY)
+  const matchData = {}
+  for (PUUID of summonerPUUIDs) {
+    const matchIDs = await getMatchIDs(PUUID)
+
+    for (const matchID of matchIDs) {
+      matchData[matchID] = await getMatchData(matchID)
+    }
+  }
+  return matchData
+}
+
+module.exports = { getSummonerData, getSummonerMatchData }
--- server/server.js
+++ server/server.js
@@ -1,17 +1,25 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+require('dotenv').config()
 
-const { getSummonerData } = require('./riotAPI')
+const { getSummonerData, getSummonerMatchData } = require('./riotAPI')
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/getBySummonerName', async (req, res) => {
+app.get('/getSummonerData', async (req, res) => {
   if (!req.body || !req.body.summonerNames) return
 
   const summonerData = await getSummonerData(req.body.summonerNames)
-  return res.json(summonerData)
+  res.status(200).json(summonerData)
+})
+
+app.get('/getSummonerMatchData', async (req, res) => {
+  if (!req.body || !req.body.summonerPUUIDs) return
+
+  const summonerMatchData = await getSummonerMatchData(req.body.summonerPUUIDs)
+  res.status(200).json(summonerMatchData)
 })
 
 const PORT = process.env.PORT || 3000
