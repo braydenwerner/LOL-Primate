4aacc8d68875712e5d71b3731c14d2c856b09aae
Brayden Werner <braydenwerner0@gmail.com>
2021-05-08 18:53:21 -0700

    Added alt tag

--- src/components/elements/Nav/Nav.tsx
+++ src/components/elements/Nav/Nav.tsx
@@ -8,7 +8,7 @@ export const Nav: React.FC = () => {
         <AppBar position="static">
             <Toolbar>
                 <NavIcon edge="start" color="inherit" aria-label="menu" >
-                    <Image src='/images/lolgorillanav.png' width={219.375} height={40.75} />
+                    <Image src='/images/lolgorillanav.png' width={219.375} height={40.75} alt='lolgorilla-title-img' />
                 </NavIcon>
             </Toolbar>
         </AppBar>
