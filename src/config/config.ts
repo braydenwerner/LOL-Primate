21d1b7c6a90a99e44ef5eb0bfcf7b8916b5aa835
Brayden Werner <braydenwerner0@gmail.com>
2021-05-08 18:42:18 -0700

    Updated production url

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const URL = dev ? 'http://localhost:3000/api' : 'https://lolgorilla.vercel.app'
+export const URL = dev ? 'http://localhost:3000/api' : `${window.location.href}/api`
